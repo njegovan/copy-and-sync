31ac7c568c88ebf49a164b7cdfa09208d3894d1e
njegovan <dev.njegovan@gmail.com>
2020-06-25 16:18:51 +0200

    init

---
+++ src/main/java/dev/njegovan/copyandsync/CopyAndSyncApplication.java
@@ -0,0 +1,13 @@
+package dev.njegovan.copyandsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyAndSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopyAndSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/njegovan/copyandsync/CopyAndSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.njegovan.copyandsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyAndSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
